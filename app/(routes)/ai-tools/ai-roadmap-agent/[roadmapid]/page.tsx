3502fb1184b2845c00ca45648c61de64a1e47f8e
kestopher <kennykestopher@gmail.com>
2025-07-28 18:30:53 +0100

    react flow install

--- app/(routes)/ai-tools/ai-roadmap-agent/[roadmapid]/page.tsx
+++ app/(routes)/ai-tools/ai-roadmap-agent/[roadmapid]/page.tsx
@@ -1,11 +1,33 @@
 'use client'
 
+import { Button } from "@/components/ui/button";
+import axios from "axios";
+import { useParams } from "next/navigation";
+import { useEffect, useState } from "react"
+
 const RoadmapGeneratorAgent = () => {
-  return (
+  const {roadmapid} = useParams()
+  const [roadmapDetails, setRoadmapDetails] = useState<any>();
+
+  useEffect(() => {
+    roadmapid && GetRoadmapDetails()
+  },[roadmapid])
+
+  const GetRoadmapDetails = async() => {
+    const result = await axios.get('/api/history?recordId=' + roadmapid)
+    console.log(result?.data);
+    setRoadmapDetails(result?.data?.content)
+  }
+  console.log(roadmapDetails, 'deets')
+
+   return (
     <div className="grid grid-cols-1 md:grid-cols-3  gap-5">
-      <div>
-        <h2 className="font-bold text-2xl"></h2>
-        <p className="mt-3 text-gray-500"><strong>Description</strong><br/></p>
+      <div className="border rounded-xl p-5">
+        <h2 className="font-bold text-2xl">{roadmapDetails?.roadmapTitle}</h2>
+        <p className="mt-3 text-gray-500"><strong>Description:</strong><br/>{roadmapDetails?.description}</p>
+        <h2 className="mt-5 font-medium text-blue-500"> Duration: {roadmapDetails?.duration}</h2>
+
+        <Button className="w-full mt-5">+ Create Another Roadmap</Button>
       </div>
       <div className="md:grid-cols-2">
         roadmap

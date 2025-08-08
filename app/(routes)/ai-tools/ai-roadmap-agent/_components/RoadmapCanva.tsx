969f136a8a46fd1f383ff99493d33fde978c4ee5
kestopher <kennykestopher@gmail.com>
2025-08-08 13:23:21 +0100

    modifying homescreen

--- app/(routes)/ai-tools/ai-roadmap-agent/_components/RoadmapCanva.tsx
+++ app/(routes)/ai-tools/ai-roadmap-agent/_components/RoadmapCanva.tsx
@@ -8,18 +8,20 @@ const nodeTypes = {
     default: TurboNode
 }
 
-const RoadmapCanva = ({initialNodes, initialEdges}:any) => {
-//     const initialNodes = [
-//   { id: 'n1', position: { x: 0, y: 0 }, data: { label: ' 1' } },
-//   { id: 'n2', position: { x: 0, y: 100 }, data: { label: ' 2' } },
-// ];
-// const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
+const RoadmapCanva = (
+    //{initialNodes, initialEdges}:any
+) => {
+    const initialNodes = [
+  { id: 'n1', position: { x: 0, y: 0 }, data: { label: ' 1' } },
+  { id: 'n2', position: { x: 0, y: 100 }, data: { label: ' 2' } },
+];
+const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
   return (
     <div style={{ width: '100%', height: '100%' }}>
       <ReactFlow
         nodes={initialNodes}
         edges={initialEdges}
-        nodeTypes={nodeTypes}
+        //nodeTypes={nodeTypes}
     >
         <Controls/>
         <MiniMap/>

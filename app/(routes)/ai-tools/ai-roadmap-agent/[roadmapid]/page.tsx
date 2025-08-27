1bcd5b2143162796ad26624b04ac17ef6367f2af
kestopher <kennykestopher@gmail.com>
2025-08-27 19:08:12 +0100

    generator fixed

--- app/(routes)/ai-tools/ai-roadmap-agent/[roadmapid]/page.tsx
+++ app/(routes)/ai-tools/ai-roadmap-agent/[roadmapid]/page.tsx
@@ -24,13 +24,17 @@ const [openRoadmapModal, setOpenRoadmapModal] = useState(false)
   console.log(roadmapDetails, 'deets')
 
    return (
-    <div className="grid grid-cols-1 md:grid-cols-3  gap-5">
+    <div className="grid grid-cols-1 md:grid-cols-3 gap-5 ">
       <div className="border rounded-xl p-5">
         <h2 className="font-bold text-2xl">{roadmapDetails?.roadmapTitle}</h2>
         <p className="mt-3 text-gray-500"><strong>Description:</strong><br/>{roadmapDetails?.description}</p>
         <h2 className="mt-5 font-medium text-blue-500"> Duration: {roadmapDetails?.duration}</h2>
 
         <Button onClick={() =>setOpenRoadmapModal(true)} className="w-full mt-5">+ Create Another Roadmap</Button>
+      </div>
+      <div className="md:col-span-2 w-full h-[80vh]">
+        {roadmapDetails && <RoadmapCanva 
+        initialNodes={roadmapDetails?.initialNodes} initialEdge={roadmapDetails?.initialEdge}/>}
       </div>
 
        <RoadmapGeneratorModal openRoadmapModal={openRoadmapModal} setOpenRoadmapModal={() => setOpenRoadmapModal(false)}/>

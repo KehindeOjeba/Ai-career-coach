8083b00e1e9cdafd159fc9dfd9b0fa4e8d413fe1
kestopher <kennykestopher@gmail.com>
2025-10-08 13:18:48 +0100

    u

--- app/(routes)/billing/page.tsx
+++ app/(routes)/billing/page.tsx
@@ -1,10 +1,12 @@
 import { PricingTable } from '@clerk/nextjs'
 import React from 'react'
+import WelcomeBanner from '../dashboard/_components/WelcomeBanner'
 
 
 const Billing = () => {
   return (
     <div className=''>
+      <WelcomeBanner/>
         <h2 className='font-bold text-3xl text-center'>Choose Your Plan</h2>
         <p className='text-lg text-center text-gray-400'>Select a subscription bundle to get all AI Tools  </p>
        <div><PricingTable/></div>
--- app/(routes)/my-history/page.tsx
+++ app/(routes)/my-history/page.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import History from '../dashboard/_components/History'
+import WelcomeBanner from '../dashboard/_components/WelcomeBanner'
 
 const MyHistory = () => {
   return (
     <div>
+      <WelcomeBanner/>
         <History/>
     </div>
   )
